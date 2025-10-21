9dabe338f12ffef169867435eb2044af28467e1b
Justin Poh <justinpohtm@gmail.com>
2025-10-21 02:29:27 +0000

    commit for ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,18 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem:"",
             }
             
+        },
+        methods:{
+            removeItem(index){
+                this.items.splice(index,1)
+            },
+            addItem(){
+                this.items.push(this.newItem)
+            }
         }
     }
 </script>
@@ -14,5 +23,11 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+        <li v-for="(item, index) in items" :key="index">{{ item }}
+            <button @click="removeItem(index)">Delete!</button></li>
+     </ul>
+     <input type="text" v-model="newItem"><button @click="addItem(newItem)">Add!</button></input>
+     
    
 </template>
