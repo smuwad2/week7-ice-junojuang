589dfa1643b2d83dea316ee4f8c3d001237f1626
Justin Poh <justinpohtm@gmail.com>
2025-10-24 11:21:43 +0000

    fix ex1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,52 +1,55 @@
 <script>
-    export default {
-        data() {
-            return {
-                x: 0,
-                y: 0,
-                selectedOp: '+',
-                operators: ['+', '-', '*', '/', '%']
-            }
-        },
-        computed: {
-            // compute the result of x (op) y without using eval
-            result() {
-                const a = Number(this.x)
-                const b = Number(this.y)
-                switch (this.selectedOp) {
-                    case '+':
-                        return a + b
-                    case '-':
-                        return a - b
-                    case '*':
-                        return a * b
-                    case '/':
-                        return b === 0 ? '∞' : a / b
-                    case '%':
-                        return b === 0 ? 'NaN' : a % b
-                    default:
-                        return 'NaN'
-                }
-            }
-        }
+export default {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      selectedOp: '+',
+      operators: ['+', '-', '*', '/', '%']
+    };
+  },
+  computed: {
+    // compute the result of x (op) y without using eval
+    result() {
+      const a = Number(this.x);
+      const b = Number(this.y);
+      switch (this.selectedOp) {
+        case '+':
+          return a + b;
+        case '-':
+          return a - b;
+        case '*':
+          return a * b;
+        case '/':
+          return b === 0 ? '∞' : a / b;
+        case '%':
+          return b === 0 ? 'NaN' : a % b;
+        default:
+          return 'NaN';
+      }
     }
-
+  }
+};
 </script>
 
 <template>
-    <p>x <input v-model.number="x"></p>
+  <div>
+    <p>x <input v-model.number="x" type="number" /></p>
     <select v-model="selectedOp">
-        <option v-for="op in operators" :value="op">{{ op }}</option>
+      <option v-for="op in operators" :key="op" :value="op">{{ op }}</option>
     </select>
-    <p>y <input v-model.number="y"></p>
-    
+    <p>y <input v-model.number="y" type="number" /></p>
     <p>---------------------</p>
-
-    <p>= {{result}}</p>
+    <p>= {{ result }}</p>
+  </div>
 </template>
 
 <style scoped>
-    p, input { font-family: monospace; }
-    p { white-space: pre; }
-
+p,
+input {
+  font-family: monospace;
+}
+p {
+  white-space: pre;
+}
 </style>
