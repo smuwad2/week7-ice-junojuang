350ff4804c3b5774864354488b9935a021335680
Justin Poh <justinpohtm@gmail.com>
2025-10-24 12:00:22 +0000

    111111

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,41 +1,47 @@
 <script>
 export default {
-    data() {
-        return {
-            x: 0,
-            y: 0,
-            selectedOp: '+',
-            operators: ['+', '-', '*', '/', '%']
-        }
-    },
-    computed: {
-        // compute the result of x (op) y without using eval
-        result() {
-            const a = Number(this.x)
-            const b = Number(this.y)
-            switch (this.selectedOp) {
-                case '+':
-                    return a + b
-                case '-':
-                    return a - b
-                case '*':
-                    return a * b
-                case '/':
-                    return b === 0 ? '∞' : a / b
-                case '%':
-                    return b === 0 ? 'NaN' : a % b
-                default:
-                    return 'NaN'
-            }
-        }
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      selectedOp: '+',
+      operators: ['+', '-', '*', '/', '%']
     }
+  },
+  computed: {
+    // compute the result of x (op) y without using eval
+    result() {
+      const a = Number(this.x)
+      const b = Number(this.y)
+      switch (this.selectedOp) {
+        case '+':
+          return a + b
+        case '-':
+          return a - b
+        case '*':
+          return a * b
+        case '/':
+          return b === 0 ? '∞' : a / b
+        case '%':
+          return b === 0 ? 'NaN' : a % b
+        default:
+          return 'NaN'
+      }
+    }
+  }
 }
 </script>
 
 <template>
-    <p>x <input v-model.number="x"></p>
-    <select v-model="selectedOp">
-        <option v-for="op in operators" :value="op">{{ op }}</option>
-    </select>
-    <p>y <input v-model.number="y"></p>
+  <div>
+    <p>x <input v-model.number="x" /></p>
+    <p>operator 
+      <select v-model="selectedOp">
+        <option v-for="op in operators" :key="op" :value="op">{{ op }}</option>
+      </select>
+    </p>
+    <p>y <input v-model.number="y" /></p>
+    <p>=</p>
+    <p>{{ result }}</p>
+  </div>
 </template>
